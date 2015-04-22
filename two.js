737a5566d7f49b028c0d63ea7f0c4a66a0d34745
Ricki Steiner <Rickisteiner@gmail.com>
2015-04-22 13:01:25 -0400

    syntax fixes

--- two.js
+++ two.js
@@ -1,6 +1,6 @@
-var thirtyOne = ["JAN", "MAR", "MAY", "JUL","AUG","OCT","DEC"]
-var thirty = ["APR","JUN","SEP","NOV"]
-var feb = "FEB"]
+var thirtyOne = ["JAN", "MAR", "MAY", "JUL","AUG","OCT","DEC"];
+var thirty = ["APR","JUN","SEP","NOV"];
+var feb = ["FEB"];
 
 var makeDates = function(thirtyOne, thirty, feb) {
 	var month = document.getElementById("month-year");
@@ -11,24 +11,33 @@ var makeDates = function(thirtyOne, thirty, feb) {
 			var daysMonth = document.create("td");
 			daysMonth.innerhtml = i;
 			days.class="unselected"
+		}
 	}
 	if(month === thirtyOne) { 
 		for (i=0; i < 31; i++) { 
 			var daysMonth = document.create("td");
 			daysMonth.innerhtml = i;
 			days.class="unselected"
+		}
 	}
 	if(month = thirty) {
 		for (i=0; i < 30; i++) { 
 			var daysMonth = document.create("td");
 			daysMonth.innerhtml = i;
 			days.class="unselected"
+		}
 	}
 }
 
-var button = document.getElementByClassName("unselected");
-button.onClick = dayStuff() {
-	if(this.class === "unselected"){ 
-		this.class = "selected";
-	}
-}
+// var button = document.getElementByClassName("unselected");
+// button.onClick = dayStuff() {
+// 	if(this.class === "unselected"){ 
+// 		this.class = "selected";
+// 	}
+// }
+
+var button = document.getElementById("next");
+
+button.addEventListener("click", function(){
+	makeDates();
+});
